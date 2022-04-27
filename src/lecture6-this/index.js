bdddd2a2c53cfde903f1fbb2a6d81645f50c3fb1
luuquangtri95 <luuquangtri95@gmail.com>
2022-04-28 04:17:17 +0700

    add note this

--- src/lecture6-this/index.js
+++ src/lecture6-this/index.js
@@ -19,7 +19,27 @@ const iphone7 = {
 
 // từ khóa this sẽ trả về đối tượng nó đang thuộc về
 
+/**
+ * todo: rất quan trọng chỗ này
+ *
+ * ! KHI KHỞI TẠO VỚI TỪ KHÓA "NEW" => SẼ XẢY RA CÁC THỨ BÊN DƯỚI
+ *
+ * todo: - Nó tạo ra một "EMPTY OBJECT" mới
+ * Nó bind this vào object mới đc tạo
+ * Nó thêm một property tên là proto vào object mới đc tạo đó, property này trỏ đến constructor của prototype của object của function
+ * Nó thêm return this vào cuối của function, do đó object mới được tạo đc return từ function
+ *
+ */
+
 function Car(name, color, weight) {
+  //! console.log(this) =>> nó là 1 object rỗng
+  /**
+   * tương tự tạo ra các key và value tương ứng
+   *
+   * object.name = "s450"
+   * object.color = "red"
+   * object.weight = 10
+   */
   this.name = name
   this.color = color
   this.weight = weight
